bff0b9360b0cb8cffe3de8ed52060951831bb89b
Brendan Kersey <bmkersey@gmail.com>
2022-08-15 18:56:03 -0400

    worked on gallery component and added some conditional rendering

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,53 +1,54 @@
-
-const categories = [
-  {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  },
-  { name: "portraits", description: "Portraits of people in my life" },
-  { name: "food", description: "Delicious delicacies" },
-  {
-    name: "landscape",
-    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  },
-];
-
-function categorySelected(name) {
-  console.log(`${name} clicked`)
-}
+import { capitalizeFirstLetter } from "../utils/helpers";
+import {useEffect} from 'react';
 
 
-function Nav() {
+
+function Nav(props) {
+  const categories = props.categories
+  console.log(categories)
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(props.currentCategory.name);
+    console.log(document.title)
+  }, [props.currentCategory]);
 
   return (
-    <header className="flex-row">
+    <header className="flex-row px-1">
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
-          <li>
+          <li className="mx-2">
             <span>Contact</span>
           </li>
-          {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
-              <span onClick={()=>categorySelected(category.name)}>
-                {category.name}
+          {categories.map(category => (
+            <li
+              className={`mx-1 ${
+                props.currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  props.setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
               </span>
             </li>
           ))}
         </ul>
       </nav>
-  </header>
+    </header>
   );
 }
+
 
 export default Nav;
