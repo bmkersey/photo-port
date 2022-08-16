d0b48db2915851a96ee547b966d48b65bd6419fa
Brendan Kersey <bmkersey@gmail.com>
2022-08-16 02:29:05 -0400

    added ability to close modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 
 
 
-const Modal = ({currentPhoto}) => {
+const Modal = ({currentPhoto, onClose}) => {
   const {name, category, description, index} = currentPhoto;
   return(
     <div className="modalBackdrop">
@@ -9,7 +9,7 @@ const Modal = ({currentPhoto}) => {
         <h3 className="modalTitle">{name}</h3>
         <img src={require(`../assets/large/${category}/${index}.jpg`)} alt="current category" />
         <p>{description}</p>
-        <button type="button">
+        <button type="button" onClick={onClose}>
           Close this modal
         </button>
       </div>
